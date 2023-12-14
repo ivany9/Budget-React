b8044209fe3fed8c33321a3ba995d37cbc3abd23
Jorge Ivan Rodriguez <ivany9@gmail.com>
2023-12-14 14:39:36 +1100

    heading

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({
+    presupuesto,
+    setPresupuesto,
+    setIsValidPresupuesto
+}) => {
+
+
+  const [mensaje,setMensaje]=useState('')
+
+    const handlePresupuesto=(e)=>{
+   e.preventDefault()
+  
+   if(!(presupuesto)|| (presupuesto)<0 )
+ {
+     setMensaje('Presupuesto no Valido')    
+     return
+ }
+ setMensaje('')   
+  setIsValidPresupuesto(true)   
+     
+    }
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+  <form onSubmit={handlePresupuesto} className='formulario'>
+  <div className='campo'>
+   <label>Definir Presupuesto</label>
+  <input 
+   className='nuevo-presupuesto'
+   type='number'
+   placeholder='Anade tu presupuesto'
+   value={presupuesto}
+   onChange={e=>setPresupuesto(Number(e.target.value))}
+     />
+ </div>
+ <input type='submit' value='anadir'/>
+    
+
+ {mensaje &&  <Mensaje tipo={"error"}>
+                         {mensaje}</Mensaje>
+                        
+       
+ }
+
+
+ 
+
+
+  </form>
+
+
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
